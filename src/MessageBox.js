23db8ea0d2a161fc195f1feb7980f2e35967ebfe
Honey Singh <simranjeet.singh@citrix.com>
2024-01-03 21:26:38 +0530

    REDIX-0002: Adding text to button

--- src/MessageBox.js
+++ src/MessageBox.js
@@ -4,17 +4,17 @@ import MessageInput from "./MessageInput";
 import store from "./store";
 
 const MessageBox = () => {
-  const { getState, subscribe } = store;
-  const [state, setState] = useState(getState());
-  
-  subscribe(() => setState(getState()));
-  
-  return (
-    <div>
-      <Messages messages={state.messages}/>
-      <MessageInput />
-    </div>
-  );
+    const { getState, subscribe } = store;
+    const [state, setState] = useState(getState());
+
+    subscribe(() => setState(getState()));
+
+    return (
+        <div>
+            <Messages messages={state.messages} />
+            <MessageInput />
+        </div>
+    );
 };
 
 export default MessageBox;
--- src/MessageInput.js
+++ src/MessageInput.js
@@ -2,26 +2,28 @@ import React, { useState } from "react";
 import store from "./store";
 
 const MessageInput = () => {
-  const {dispatch} = store;
+  const { dispatch } = store;
   const [inputText, setInputText] = useState("");
-  
+
   const updateInputValue = (evt) => {
     setInputText(evt.target.value);
   };
-  
+
   const onClickHandlerAddMessage = () => {
     dispatch({
       type: "AddMessage",
       message: inputText
     });
-    
+
     setInputText("");
   };
-  
+
   return (
     <div>
-      <input value={inputText} onChange={updateInputValue}/>
-      <button icon="add" onClick={onClickHandlerAddMessage} />
+      <input value={inputText} onChange={updateInputValue} />
+      <button icon="add" onClick={onClickHandlerAddMessage}>
+        Add Message
+      </button>
     </div>
   );
 };
