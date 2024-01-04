cf41a6d511d91b5270a77ea66e2fc3c8679c2bb1
Honey Singh <simranjeet.singh@citrix.com>
2024-01-04 22:11:47 +0530

    REDUX-0004: Doing some styling to the Messages

--- src/Messages.js
+++ src/Messages.js
@@ -13,12 +13,23 @@ const Messages = (props) => {
         return new Date(a.timeStamp) - new Date(b.timeStamp);
     });
     return (
-        <ul className="messages">
+        <ul className="messages"
+            style={{
+                listStyleType: "none",
+                display: "flex",
+                flexDirection: "column",
+                backgroundColor: "#e8e8e8",
+                padding: "8px",
+                }}>
             {
                 messages.map((message, index) => {
                     return (
                         <li
-                            style={{ textAlign: props.from === message.from ? "right" : "left" }}
+                            style={{
+                                alignSelf: props.from === message.from ? "end" : "start",
+                                backgroundColor: props.from === message.from ? "lightgreen" : "white",
+                                margin: "8px 0"
+                            }}
                             key={index}
                             onClick={() => dispatch({
                                 type: "DeleteMessage",
